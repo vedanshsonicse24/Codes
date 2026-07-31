e26a1f590e676fe479a2384c8a8f09a0bab9e823
Vedansh Soni <vedansh.sonicse24@ssipmt.com>
2026-07-31 23:58:57 +0530

    Time: 8 ms (61.48%), Space: 25.2 MB (96.15%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26,0);
+
+        for(int i=0; i<word.size();i++){
+            freq[word[i]-'a']++;
+        }
+        sort(freq.begin(), freq.end(), greater<int>());
+        int answer =0;
+        for(int i=0; i<26; i++){
+                if(freq[i]==0){
+                    break;
+                }
+            int pushes =(i/8)+1;
+            answer += freq[i] * pushes;
+            }
+            return answer;
+    }
+};
